16b19b87f8e78123e059028e0626c8c99ed97b0b
jessicaseniw <jseniwborges@byupathway.edu>
2026-03-10 22:30:42 -0300

    defect 1 - index need to start in 0, not 1. defect 2 - need to add the value to the end of the queue.

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -53,8 +53,9 @@
     /// Enqueue the value provided into the queue
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
+    
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+    _queue.Add(value);
     }
 
     /// <summary>
@@ -65,9 +66,8 @@
     private int Dequeue() {
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
-
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0];
+        _queue.RemoveAt(0);
         return value;
     }
 }
